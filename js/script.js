c50d6e44198d2524e99eddb34bf931c0cdaedda0
federicomanni97 <manni_federico@libero.it>
2023-10-27 15:16:20 +0200

    bonus image added

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ const { createApp } = Vue
     data() { //con dentro un altro metodo che si chiama data
       return { //deve ritornare
         title: 'Hello World!', //una variabile con un messaggio con scritto Hello Vue!
-        red: 'blue'
+        white: 'white',
+        bgImage: "../img/space-background.jpg",
       }
     },
   }).mount('#app'); //monta i miei dati dentro il div con l'id "app"
